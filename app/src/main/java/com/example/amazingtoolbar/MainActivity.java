76f1db5b9cbdf870db2e20f012a98bb04ec13963
Nikhil <nikhil725051>
2022-03-14 13:58:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/amazingtoolbar/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.amazingtoolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
